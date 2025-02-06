a816acdf16d0efada70a4c33f4b0d56d1741f2f0
shasin theekshana <shasintheekshana2001@gmail.com>
2025-02-07 00:07:24 +0530

    Initialize project with basic Tkinter GUI

---
+++ password_gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Create main window
+root = tk.Tk()
+root.title("Password Generator")
+root.geometry("400x300")
+root.resizable(False, False)
+
+# Run the application
+root.mainloop()
